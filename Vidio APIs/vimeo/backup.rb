4e8602b9c360e13173c50278eab69b9847856edb
bradyJ89 <omfguess@aim.com>
2014-04-01 15:42:45 -0400

    cleanup

--- Vidio APIs/vimeo/backup.rb
+++ Vidio APIs/vimeo/backup.rb
@@ -6,7 +6,7 @@ require 'active_support/all'
 require 'oauth'
 
 
-class Vimeo
+class VimeoVideos
 	include HTTParty
 	base_uri 'vimeo.com'
 end
@@ -60,30 +60,19 @@ categories.each do |cat_key, value|
 				if upload_date > 1.day.ago.to_s
 						response = access_token.get('/api/rest/v2?format=json&method=vimeo.videos.getInfo&video_id=' + id)
 						vid_info = JSON.parse(response.body)
-						#p vid_info['video'][0]['title'], 
-						#	vid_info['video'][0]['description'] , 
-						#	vid_info['video'][0]['upload_date'], 
-						#	vid_info['video'][0]['number_of_plays'], 
-						#	vid_info['video'][0]['number_of_likes'], 
-						#	vid_info['video'][0]['duration']
+						p vid_info['video']
 						views = vid_info['video'][0]['number_of_plays']
 						if views.to_i > 100
 							p views
 							vidios.push(id)
 							yesterday.push(id)
-							#p views
+							p views
 							p "total: " + vidios.uniq.length.to_s, "yesterday: " + yesterday.uniq.length.to_s, "last week: " + last_week.uniq.length.to_s, "last month: " + last_month.uniq.length.to_s
 						end
 					elsif upload_date > 1.week.ago.to_s
 						response = access_token.get('/api/rest/v2?format=json&method=vimeo.videos.getInfo&video_id=' + id)
 						vid_info = JSON.parse(response.body)
-						#p vid_info['video']
-						#p vid_info['video'][0]['title'], 
-						#	vid_info['video'][0]['description'] , 
-						#	vid_info['video'][0]['upload_date'], 
-						#	vid_info['video'][0]['number_of_plays'], 
-						#	vid_info['video'][0]['number_of_likes'], 
-						#	vid_info['video'][0]['duration']
+						p vid_info['video']
 						views = vid_info['video'][0]['number_of_plays']
 						if views.to_i > 1000
 							vidios.push(id)
@@ -94,13 +83,7 @@ categories.each do |cat_key, value|
 					elsif upload_date > 1.month.ago.to_s
 						response = access_token.get('/api/rest/v2?format=json&method=vimeo.videos.getInfo&video_id=' + id)
 						vid_info = JSON.parse(response.body)
-						#p vid_info['video']
-						#p vid_info['video'][0]['title'], 
-						#	vid_info['video'][0]['description'] , 
-						#	vid_info['video'][0]['upload_date'], 
-						#	vid_info['video'][0]['number_of_plays'], 
-						#	vid_info['video'][0]['number_of_likes'], 
-						#	vid_info['video'][0]['duration']
+						p vid_info['video']
 						views = vid_info['video'][0]['number_of_plays']
 						if views.to_i > 10000
 							vidios.push(id)
@@ -116,30 +99,3 @@ categories.each do |cat_key, value|
 	end
 end
 p "total: " + vidios.uniq.length.to_s, "yesterday: " + yesterday.uniq.length.to_s, "last week: " + last_week.uniq.length.to_s, "last month: " + last_month.uniq.length.to_s
-
-=begin
-#trying to use beta..doesn't work
-
-client = OAuth2::Client.new(
-	CONSUMER_KEY, 
-	SECRET, 
-	:site => 'http://api.vimeo.com'
-)
-
-access_token = OAuth2::AccessToken.new(client, TOKEN)
-p access_token
-#video = access_token.get('/categories/')
-#p video
-#p client
-#auth_url = client.auth_code.authorize_url(:redirect_uri => 'http://api.vimeo.com/oauth2/callback')
-# => "https://example.org/oauth/authorization?response_type=code&client_id=client_id&redirect_uri=http://localhost:8080/oauth2/callback"
-#p auth_url
-#token = client.auth_code.get_token(auth_url, :redirect_uri => 'http://api.vimeo.com/oauth2/callback', :headers => {'Authorization' => 'Basic ' + SECRET})
-#response = token.get('/api/resource', :params => { 'query_foo' => 'bar' })
-#p token
-#response.class.name
-#result = Vimeo.get('/channels/staffpicks/videos?filter=content_rating&filter_content_rating=safe,unrated')
-#p result
-#response = JSON.parse(result.body)
-#p response
-=end
