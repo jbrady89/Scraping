3c120a09a686109ff63e1643ec0f48878994e7c1
bradyJ89 <omfguess@aim.com>
2014-04-03 19:38:50 -0400

    waiting for response from vimeO

--- Vidio APIs/vimeo/vimeoBeta.rb
+++ Vidio APIs/vimeo/vimeoBeta.rb
@@ -8,28 +8,60 @@ require 'oauth2'
 CLIENT_ID = "bebedacec262f1c8490fe8efbee259a9547898eb"
 CLIENT_SECRET = "11ae1c7df03482894ac3d665140d270d75c2f766"
 TOKEN = "edb561bc411ac2ac53b482cd29a9c9be"
+
+categories = 	{
+					'Activism & Non Profits' => 'activism',
+					'Animation & Motion Graphics' => 'animation', 
+					'Art' => 'art',
+					'Branded' => 'branded', 
+					'Comedy' => 'comedy',
+					'Education & DIY' => 'education', 
+					'Everyday Life' => 'everyday',
+					'Experimental' => 'experimental',
+					'Fashion' => 'fashion',
+					'Films' => 'films',
+					'HD' => 'hd',
+					'Music' => 'music',
+					'Nature' => 'nature',
+					'Products & Equipment' => 'productsandequipment',
+					'Science & Tech' => 'technology',
+					'Sports' => 'sports',
+					'Travel & Events' => 'travel',
+					'Vimeo Projects' => 'vimeoprojects',
+					'Web Series' => 'webseries'
+				}
+
 count = 0
+last_week = []
+last_month = []
+yesterday = []
 site_path = 'https://api.vimeo.com'
-redirect_uri = 'http://szl.it'
 client = OAuth2::Client.new(CLIENT_ID, CLIENT_SECRET, {:site => site_path, :signature_method => "HMAC-SHA1", :scheme => :header })
 token = OAuth2::AccessToken.new(client, TOKEN)
-for i in 1..100
-	page_number = i.to_s
-	videos = token.get('/categories/music/videos?sort=plays&page=' + page_number + '&per_page=100')
-	hash = JSON.parse(videos.body)
-	hash["data"].each do |a|
-		if a['created_time'] > 1.month.ago.to_s && a['stats']['plays'].to_i > 10000
-			p a['stats'], a['created_time'], "last_month"
-			count += 1
-			elsif a['created_time'] > 1.week.ago.to_s && a['stats']['plays'].to_i > 1000
-				p a['stats'], a['created_time'], "last_week"
+
+categories.each do |category_name, cat_uri|
+	for i in 1..100
+		page_number = i.to_s
+		videos = token.get('/categories/' + cat_uri + '/videos?sort=plays&page=' + page_number + '&per_page=100')
+		hash = JSON.parse(videos.body)
+		hash["data"].each do |a|
+			if a['created_time'] > 1.month.ago.to_s && a['stats']['plays'].to_i > 10000
+				p a['stats'], a['created_time'], "last_month"
+				last_month.push(a['created_time'])
 				count += 1
-			elsif a['created_time'] > 1.day.ago.to_s && a['stats']['plays'].to_i > 100
-				p a['stats'], a['created_time'], "yesterday"
-				count += 1
+				elsif a['created_time'] > 1.week.ago.to_s && a['stats']['plays'].to_i > 1000
+					p a['stats'], a['created_time'], "last_week"
+					last_week.push(a['created_time'])
+					count += 1
+				elsif a['created_time'] > 1.day.ago.to_s && a['stats']['plays'].to_i > 100
+					p a['stats'], a['created_time'], "yesterday"
+					yesterday.push(a['created_time'])
+					count += 1
+			end
 		end
 	end
 end
+p "Month: " + last_month.uniq.length, "Week: " + last_week.uniq.length, "yesterday: " + yesterday.uniq.length
 p count += 1
 #code = client.auth_code.authorize_url(:redirect_uri => "http://localhost:3000")
 #p code
