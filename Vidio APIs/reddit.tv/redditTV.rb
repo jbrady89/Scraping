4b38c413d224c01d6bb1d2006b076da17ff89621
bradyJ89 <omfguess@aim.com>
2014-04-07 20:35:44 -0400

    fix class

--- Vidio APIs/reddit.tv/redditTV.rb
+++ Vidio APIs/reddit.tv/redditTV.rb
@@ -1,56 +1,37 @@
-require 'open-uri'
-require 'nokogiri'
-require 'HTTParty'
-
-channels = [
-			"videos", 
-			"music", 
-			"vicevideo", 
-			"television", 
-			"games", 
-			"sports", 
-			"documentaries", 
-			"fullmoviesonyoutube", 
-			"redditpicks", 
-			"thenewyorktimes", 
-			"fringediscussion", 
-			"kidsafevideos", 
-			"listentothis", 
-			"hiphopheads", 
-			"classicalmusic", 
-			"jazz", 
-			"sciencevideos", 
-			"todayilearned", 
-			"learnuselesstalents", 
-			"deepintoyoutube"
-		]
-
-
 class Reddit
+	Channels = [
+					"videos", 
+					"music", 
+					"vicevideo", 
+					"television", 
+					"games", 
+					"sports", 
+					"documentaries", 
+					"fullmoviesonyoutube", 
+					"redditpicks", 
+					"thenewyorktimes", 
+					"fringediscussion", 
+					"kidsafevideos", 
+					"listentothis", 
+					"hiphopheads", 
+					"classicalmusic", 
+					"jazz", 
+					"sciencevideos", 
+					"todayilearned", 
+					"learnuselesstalents", 
+					"deepintoyoutube"
+				]
 
-	def getLinks channels
+	def get_links
 		video_id = []
-		channels.each do |channel_link|
+		Channels.each do |channel_link|
 			channel_uri = "#/r/" + channel_link
-			output = `phantomjs hello.js #{channel_uri} &`
+			output = `phantomjs rtv.js #{channel_uri} &`
 			output.gsub!(/\n/, '')
 			links = output.split(',')
 			p links
 			links.each do |page_link|
 				video_id.push(page_link)
-				#p page_link
-=begin
-				embedLink = `phantomjs player.js #{page_link} &`
-				#p embedLink 
-				unless embedLink[0..3] == "null"
-					id = embedLink[30..40]
-					p "link: " + embedLink.gsub!(/\n/, '')
-					p "id: " + id
-					count += 1
-					p count
-					video_id.push(id)
-				end
-=end
 			end
 		end
 		p "Total: " + video_id.uniq.length.to_s 
@@ -58,4 +39,5 @@ class Reddit
 end
 
 reddit = Reddit.new
-reddit.getLinks(channels)
+#p reddit.class_variables
+reddit.get_links
