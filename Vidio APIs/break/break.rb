b962aee042c2ce16e1666c7bcfb38b9d254cd2b5
bradyJ89 <omfguess@aim.com>
2014-04-27 15:39:53 -0400

    add metacafe top 5

--- Vidio APIs/break/break.rb
+++ Vidio APIs/break/break.rb
@@ -2,6 +2,12 @@ require 'httparty'
 require 'open-uri'
 require 'nokogiri'
 
+class Metacafe
+	include HTTParty
+	base_uri 'http://metacafe.com'
+end
+=begin
+#BREAK
 xml = Nokogiri::HTML( open( "http://rss.break.com" ) )
 xml.xpath( "//item/*" ).each do |a|
 	if a.name.to_s == 'description'
@@ -12,3 +18,31 @@ xml.xpath( "//item/*" ).each do |a|
 	end
 	p a.name.to_s + ": " + a.text
 end
+=end
+
+#METACAFE Top 5
+links = []
+html = Nokogiri::HTML( open( "http://www.metacafe.com/popular-now/" ) )
+html.xpath("//section[@class='Spotlight Mode5v']//li").each do |a|
+	link_text = a.css('a').attr('href').text
+	links.push(link_text)
+end
+#p links.uniq
+links.each do |link|
+	video_tags = []
+	link = 'http://metacafe.com' + link
+	html = Nokogiri::HTML( open( link ) )
+	#p html.css("hgroup#ItemTitle").text, html.css("h2#Views").text, html.css('h2#UploadInfo').text, html.css('div#Description').text, 
+	tags = html.css('dl#Tags > dd > a')
+	tags.each do |tag|
+		video_tags.push(tag.text)
+	end
+	p video_tags
+end
+
+=begin
+#embed link
+<iframe src="http://www.metacafe.com/embed/10859663/" width="440" height="248" allowFullScreen frameborder=0></iframe>	
+=end
+
+
