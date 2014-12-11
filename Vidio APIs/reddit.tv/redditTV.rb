744ebba3889901067953e937a4c781abcc81e70c
bradyJ89 <omfguess@aim.com>
2014-12-10 19:00:26 -0500

    added comments

--- Vidio APIs/reddit.tv/redditTV.rb
+++ Vidio APIs/reddit.tv/redditTV.rb
@@ -6,28 +6,28 @@ class Youtube
 end
 
 class Reddit
-	Reddit_channels = [
-					"videos", 
-					"music", 
-					"vicevideo", 
-					"television", 
-					"games", 
-					"sports", 
-					"documentaries", 
-					"fullmoviesonyoutube", 
-					"redditpicks", 
-					"thenewyorktimes", 
-					"fringediscussion", 
-					"kidsafevideos", 
-					"listentothis", 
-					"hiphopheads", 
-					"classicalmusic", 
-					"jazz", 
-					"sciencevideos", 
-					"todayilearned", 
-					"learnuselesstalents", 
-					"deepintoyoutube"
-				]
+	Reddit_channels = 	[
+							"videos", 
+							"music", 
+							"vicevideo", 
+							"television", 
+							"games", 
+							"sports", 
+							"documentaries", 
+							"fullmoviesonyoutube", 
+							"redditpicks", 
+							"thenewyorktimes", 
+							"fringediscussion", 
+							"kidsafevideos", 
+							"listentothis", 
+							"hiphopheads", 
+							"classicalmusic", 
+							"jazz", 
+							"sciencevideos", 
+							"todayilearned", 
+							"learnuselesstalents", 
+							"deepintoyoutube"
+						]
 
 	def self.get_reddit_links
 		video_id = []
@@ -38,7 +38,10 @@ class Reddit
 			output = `phantomjs rtv.js #{channel_uri} &`
 			#p output
 			#video_id.push(output)
+			# remove new line characters
+
 			output.gsub!(/\n/, '')
+			# create an array using the output
 			links = output.split(',')
 			links.each do |page_link|
 				video_id.push page_link
@@ -54,27 +57,35 @@ p output
 #p output.length.to_s
 #p output.uniq
 count = 0
+
+# iterate over each item in the output array
+# if it is a youtube link, more data is fetched using the youtube data API
 output.each do |id|
 
-	
-	snippet =	Youtube.get('/videos?part=snippet&id=' + id + '&key=AIzaSyBi5KmDUjrcysyFgQgTddYMx0bJgGPxjFQ')
-	details =	Youtube.get('/videos?part=contentDetails&id=' + id + '&key=AIzaSyBi5KmDUjrcysyFgQgTddYMx0bJgGPxjFQ')
+	snippet    =	Youtube.get('/videos?part=snippet&id=' + id + '&key=AIzaSyBi5KmDUjrcysyFgQgTddYMx0bJgGPxjFQ')
+	details    =	Youtube.get('/videos?part=contentDetails&id=' + id + '&key=AIzaSyBi5KmDUjrcysyFgQgTddYMx0bJgGPxjFQ')
 	statistics =	Youtube.get('/videos?part=statistics&id=' + id + '&key=AIzaSyBi5KmDUjrcysyFgQgTddYMx0bJgGPxjFQ')
 	
 
 	[ snippet,  details, statistics ].each do |vid_info|
 		begin
+			# continue unless the query returns nothing
+			# this will be the case for non youtube videos
 			unless vid_info == nil
+
 				parsed = JSON.parse(vid_info.body)
-				#p response, '---', response['items']
 				hash = parsed['items']
 				p hash == nil
+
+				# unless there is no information
+				# print the data
 				unless hash == nil
 					count += 1
 					p "data: " + hash[0].to_s
 				end
 			end
 		rescue
+			# go to the next item if current one is a non YT link
 			next
 		end
 	end
