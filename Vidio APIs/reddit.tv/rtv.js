f04e03beb332f1dbef2af65a5fdd7611e3983b89
bradyJ89 <omfguess@aim.com>
2014-12-10 19:00:40 -0500

    added comments

--- Vidio APIs/reddit.tv/rtv.js
+++ Vidio APIs/reddit.tv/rtv.js
@@ -1,7 +1,10 @@
 var system = require('system'),
+	// to access the parameters specified in the command line
 	uri = system.args[1],
 	page = require('webpage').create(),
+	// store youtube links
 	urls = [],
+	// everything else
 	non_yt = [],
 	url = "http://reddit.tv/" + uri;
 	
@@ -15,10 +18,14 @@ page.open(url, function(status) {
 			//console.log(return link.getAttribute('href'));
 			// this examines the urls to determine what type of video it is (youtube, vimeo, etc)
 			if (link.style['backgroundImage'].substring(11,15) == 'edge') {
+
 				return link.style['backgroundImage'].substring(61,73);
 			}	else if (link.style['backgroundImage'].substring(11,15) == 'i.vi') {
+				// vimeo
 				return link.style['backgroundImage'].substring(32, 41);
 			}	else if (link.style['backgroundImage'].substring(11,15) == 'i2.y') {
+				// this grabs the video ID
+				// we use this for getting more data via the youtube API in redditTV.rb
 				return link.style['backgroundImage'].substring(27, 38);
 			} else  {
 				//non_yt.push(link.style['backgroundImage']);
@@ -26,15 +33,20 @@ page.open(url, function(status) {
 			}
         });
     });
+    console.log(links);
+    console.log(links.length)
 
     // iterate through the array
+    // not sure why I copied everything from one array to another?
 	for (i = 0; i < links.length; i++){
 		url = links[i];
 		urls.push(url);
 	}
+
 	// this outputs the contents of the urls array
 	// for use in the redditTv.rb file on line 
-	console.log(urls);
+	//console.log(urls);
+	console.log(links.length);
     phantom.exit();
 });
 
