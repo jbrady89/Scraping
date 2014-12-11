25b00246e31eb69285c7474dc6450c9c0c93a3c1
bradyJ89 <omfguess@aim.com>
2014-12-10 19:07:38 -0500

    fix broken query

--- Vidio APIs/dailymotion/dailyMotion.rb
+++ Vidio APIs/dailymotion/dailyMotion.rb
@@ -12,6 +12,8 @@ channels = [ "music", "fun", "shortfilms", "news", "sport", "videogames", "auto"
 timeframe = { "today" => 1.day.ago, "last_week" => 1.week.ago, "last_month" => 1.month.ago }
 vidio_ids = []
 count = 0
+start = Time.now
+p "start time: " + start.to_s
 timeframe.each do |time_key, time_period|
 	time = time_period.strftime("%m/%d/%Y")
 	p time_key
@@ -19,7 +21,8 @@ timeframe.each do |time_key, time_period|
 		#p channel
 		for n in 1..100
 			pageNumber = n.to_s
-			channel_vidios = DailyMotion.get('/channel/' + channel + '/videos?fields=allow_embed,channel.name%2Ccomments_total%2Ccreated_time%2Cdescription%2Cduration%2Cid%2Cowner.url%2Cowner.username%2Crating%2Cratings_total%2Ctags%2Cthumbnail_720_url%2Ctitle%2Curl%2Cviews_total&created_after=' + time + '&language=EN&sort=visited&page=' + pageNumber + '&limit=100')
+			#videos?fields=allow_embed,channel.description%2Cchannel.name%2Ccomments_total%2Cdescription%2Cduration%2Cid%2Clanguage%2Cowner.url%2Ctitle%2Curl%2Cviews_total%2C&created_after=12%2F1%2F2014&lang=en&sort=visited
+			channel_vidios = DailyMotion.get('/channel/' + channel + '/videos?fields=allow_embed,channel.description%2Cchannel.name%2Ccomments_total%2Cdescription%2Cduration%2Cid%2Clanguage%2Cowner.url%2Ctitle%2Curl%2Cviews_total%2C&created_after=' + time + '&language=en&sort=visited&page=' + pageNumber + '&limit=100')
 			channel_vidios['list'].each do |a|
 				unless a.any? == false
 					vidio_id = a['id']
@@ -28,18 +31,18 @@ timeframe.each do |time_key, time_period|
 					if viewCount > 100000 && time_key == "last_month"
 						vidio_ids.push(vidio_id)
 						count += 1
-						p count
+						#p count
 						#p a, "---"
 						#p a['ratings_total'], a['rating']
 						elsif viewCount > 10000 && time_key == "last_week"
 							vidio_ids.push(vidio_id)
 							count += 1
-							p count
+							#p count
 							#p a, "---"
 						elsif viewCount > 1000 && time_key == "today"
 							vidio_ids.push(vidio_id)
 							count += 1
-							p count
+							#p count
 							#p a, "---"
 					end
 				end
@@ -47,6 +50,11 @@ timeframe.each do |time_key, time_period|
 		end
 	end
 end
+
+time_finish = Time.now
+elapsed = (time_finish - start) / 60
+p "end time: " + time_finish.to_s
+p "total time: " + elapsed.to_s + "minutes"
 p "videos: " + vidio_ids.uniq.length.to_s
 
 
