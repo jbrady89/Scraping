7a8a99c41ca8a5973f6cc8aeb88feb6f381e8d73
bradyJ89 <omfguess@aim.com>
2015-01-19 00:55:51 -0500

    Tweet class added for saving tweet/tweet data to the db

--- Scrapers/twitter/TwitTrade/twitterStream.py
+++ Scrapers/twitter/TwitTrade/twitterStream.py
@@ -1,8 +1,10 @@
 import datetime, threading, time, json, requests, psycopg2, sys
 from tweepy import Stream, OAuthHandler, StreamListener
 from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import DateTime, Time
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from decimal import *
 
 #Define our connection string
 #conn_string = 'database="twitter", port="5433", user="twitter", password="password"'
@@ -29,22 +31,23 @@ class Price(Base):
 	id = Column(Integer, primary_key=True)
 	symbol = Column(String)
 	price = Column(Integer)
-	time = Column(Integer)
+	date = Column(DateTime, default=datetime.datetime.now())
 
 	def __repr__(self):
-		return "<User(symbol='%s', price='%s', time='%s')>" % (self.symbol, self.price, self.time)
+		return "<User(symbol='%s', price='%s', time='%s')>" % (self.symbol, self.price, self.date)
 
 class Tweet(Base):
 	__tablename__ = 'tweets'
 
 	id = Column(Integer, primary_key=True)
-	author = Column(String)
+	user = Column(String)
 	followers = Column(Integer)
+	following = Column(Integer)
 	text = Column(String)
-	time = Column(Integer)
+	time = Column(DateTime, default=datetime.datetime.now())
 
 	def __repr__(self):
-		return "<User(symbol='%s', price='%s', time='%s')>" % (self.symbol, self.price, self.time)
+		return "<User(symbol='%s', price='%s', time='%s')>" % (self.user, self.followers, self.text, self.time)
 
 
 
@@ -57,61 +60,82 @@ count = 0
 next_call = time.time()
 last_price = None
 
-#can get minute data for up to the last 15 days
-def get_historic_data():
-	response = requests.get("http://chartapi.finance.yahoo.com/instrument/1.1/AAPL/chartdata;type=quote;range=15d;/json/")
+#gets minute data for up to the last 15 days
+def get_historic_data(symbol):
+	response = requests.get("http://chartapi.finance.yahoo.com/instrument/1.1/{}/chartdata;type=quote;range=15d;/json/".format(symbol))
 	jsonp = response.text
 	fixed_json = jsonp[ jsonp.index("(") + 1 : jsonp.rindex(")") ]
 	price_data = json.loads(fixed_json)
+	#series is the location of the minutely price data
 	print(price_data['series'])
 
-#get_historic_data()
+#get_historic_data("AAPL")
 
 def get_stock_data():
 	global last_price
 	response = requests.get("http://finance.yahoo.com/webservice/v1/symbols/AAPL/quote?format=json")
 	json_data = response.json()
 
-	timestamp = json_data['list']['resources'][0]['resource']['fields']['utctime']
+	time = json_data['list']['resources'][0]['resource']['fields']['utctime']
 	symbol = json_data['list']['resources'][0]['resource']['fields']['symbol']
 	price = json_data['list']['resources'][0]['resource']['fields']['price']
-
-	new_price = Price( symbol=symbol, price=float(price), time=time.time())
+	p = Decimal(price)
+	# only display two places
+	p = round(p, 2)
+	print(p, price)
+	print()
+	new_price = Price( symbol=symbol, price=p )
 
 	# insert the new data in db
 	session.add(new_price)
 	session.commit()
+	print("record saved!")
 	#data = {"time": time, "symbol": symbol, "price": price, "date": datetime.datetime.utcnow() }
 	#prices.insert(data)
 	if (last_price):
 	  difference = int(float(price)) - int(float(last_price))
 	  #print(" change: {}".format(difference))
-	  print(" time: {}, symbol: {}, price: {}, change: {}, \n".format(timestamp, symbol, price, difference))
+	  print(" time: {}, symbol: {}, price: {}, change: {}, \n".format(time, symbol, price, difference))
+	  return
 
 	print()
 	print(" time: {}, symbol: {}, price: {}, \n".format(time, symbol, price))
 	last_price = price
 
 def get_price():
-  global next_call
+  global next_call #the current time
   print ( datetime.datetime.now() )
-  get_stock_data()
-  next_call = next_call+60
+  get_stock_data() #get the current price
+  next_call = next_call+60 #schedule the next call for 1 minute in the future
+  #this sets up the 1 minute interval running in the background
   threading.Timer( next_call - time.time(), get_price ).start()
 
-def sentAnalysis(text):
+def sentAnalysis(user, followers, following, text):
 	global count
 	count += 1
-	print("count: {}, text: {}, \n".format(count, text))
+	new_tweet = Tweet( user=user, followers=followers, following=following, text=text )
 
+	# insert the new data in db
+	session.add(new_tweet)
+	session.commit()
+	print("record saved!")
+
+	print("user: {}, followers: {}, following: {}, text: {}, \n".format(user, followers, following, text))
+	print("active threads: {}".format(threading.active_count))
 
 class listener(StreamListener):
 
     def on_data(self, data):
     	
         tweet = json.loads(data)
-        tweet_text = tweet['text']
-        sentAnalysis(tweet_text)
+       	#print( json.dumps( tweet, sort_keys=True, indent=4, separators=(',', ': ') ) )
+
+        user = tweet['user']['screen_name']
+        followers = tweet['user']['followers_count']
+        following = tweet['user']['friends_count']
+        text = tweet['text']
+
+        sentAnalysis(user, followers, following, text)
 
     def on_error(self, status):
         print( status.text )
@@ -121,4 +145,4 @@ auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 get_price()
 twitterStream = Stream(auth, listener())
-twitterStream.filter(track=["$AAPL", "$FB"])
+twitterStream.filter(track=["$AAPL"])
