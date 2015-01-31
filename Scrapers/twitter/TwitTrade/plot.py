0c8a6e1a7aecb32da3792e5ce657ac85e82827e7
bradyJ89 <omfguess@aim.com>
2015-01-31 00:30:30 -0500

    delete old files

--- Scrapers/twitter/TwitTrade/plot.py
+++
@@ -1,8 +0,0 @@
-import datetime, threading, time, json, requests
-from tweepy import Stream, OAuthHandler, StreamListener
-from sqlalchemy import create_engine, Column, Integer, Float, Text, Boolean
-from sqlalchemy import DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from textblob import TextBlob
-import langid
