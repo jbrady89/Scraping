d67f3851ebc6a108050ecdcd4accb4509b65dab3
jbrady89 <omfguess@aim.com>
2016-07-08 09:22:02 -0400

    substitute keys with string variable

--- final/redditTV.rb
+++ final/redditTV.rb
@@ -55,9 +55,9 @@ count = 0
 output.each do |id|
 
 	
-	snippet =	Youtube.get('/videos?part=snippet&id=' + id + '&key=AIzaSyBi5KmDUjrcysyFgQgTddYMx0bJgGPxjFQ')
-	details =	Youtube.get('/videos?part=contentDetails&id=' + id + '&key=AIzaSyBi5KmDUjrcysyFgQgTddYMx0bJgGPxjFQ')
-	statistics =	Youtube.get('/videos?part=statistics&id=' + id + '&key=AIzaSyBi5KmDUjrcysyFgQgTddYMx0bJgGPxjFQ')
+	snippet =	Youtube.get('/videos?part=snippet&id=' + id + '&key=#{key}')
+	details =	Youtube.get('/videos?part=contentDetails&id=' + id + '&key=#{key}')
+	statistics =	Youtube.get('/videos?part=statistics&id=' + id + '&key=#{key}')
 	
 
 	[ snippet,  details, statistics ].each do |vid_info|
